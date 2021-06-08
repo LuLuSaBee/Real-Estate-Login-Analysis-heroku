adc843a03f3670aa794c733404a4abdb2a92c6a7
lulusabee <lulusabeetw@gmail.com>
2021-06-08 16:13:49 +0800

    react init

--- finalproject/finalproject/urls.py
+++ finalproject/finalproject/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mainsite.views import homepage
+from frontend.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', homepage),
+    path('', index),
 ]
